dc52391fede4adfe3891eaaeb2426be8105ffc1a
Joseph Dubon <josephdubon@pm.me>
2020-05-12 20:24:28 -0700

    Adjustments to variable from 0 to 1, getting closer to desired result.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // output the header row
 document.write('<span class="cell header">&times;</span>')
-let i = 0
+let i = 1
 
 while (i <= 10) {
     document.write('<span class="cell header">' + i + "</span>")
@@ -9,15 +9,11 @@ while (i <= 10) {
 document.write("<br>")
 // TODO: Write two nested while loops to output the rest of the multiplication table
 
-let j
-let total
+let j = 0
 
 for (i = 0; i <= 10; i++) {
     for (j = 0; j <= 10; j++) {
-        total = i * j
-        total = " " + total
-        if (total <= 10) total = " " + total
-        document.write('<span class="cell">' + total + "</span>")
+        document.write('<span class="cell">' + j * i + "</span>")
         }
         document.write("<br>")
     }
