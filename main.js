442e74de01968d4e4ccf7dd29f7a5a4ca75494ec
Joseph Dubon <josephdubon@pm.me>
2020-05-13 10:36:26 -0700

    Redoing table with help. Created two headers for table in main.js.

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
-// output the header row
-document.write('<span class="cell header">&times;</span>')
-let i = 1
-
+// OUTPUT THE HEADER ROW
+document.write('<span class="cell header">&times;</span>');
+let i = 0;
 while (i <= 10) {
-    document.write('<span class="cell header">' + i + "</span>")
-    i++
+    document.write('<span class="cell header">' + i + "</span>");
+    i++;
 }
-document.write("<br>")
-// TODO: Write two nested while loops to output the rest of the multiplication table
+// OUTPUT EACH BODY ROWS
 
-let j = 0
+// 1: INITIALIZE A LOOP VARIABLE
+// 2: CONDITIONAL (EXIT PLAN)
+// 3: INCREMENT THE LOOP VARIABLE
 
-for (i = 0; i <= 10; i++) {
-    for (j = 0; j <= 10; j++) {
-        document.write('<span class="cell">' + j * i + "</span>")
-        }
-        document.write("<br>")
-    }
+let rowNum = 1
+while (rowNum <= 10) {
+    document.write('<span class="cell header">' + rowNum + '</span>')
+    document.write('<br>')
+    rowNum++
+}
+
+
+// let j = 1
+
+// for (i = 0; i <= 10; i++) {
+//     for (j = 0; j <= 10; j++) {
+//         document.write('<span class="cell">' + j * i + "</span>")
+//         }
+//         document.write("<br>")
+//     }
