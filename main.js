95920f90c9cd98b0086c685e9ea082a631bde9af
Joseph Dubon <josephdubon@pm.me>
2020-05-13 11:13:57 -0700

    Cleaned up code, globally, for better reading. Changed background color of header.

--- main.js
+++ main.js
@@ -1,31 +1,23 @@
 // OUTPUT THE HEADER ROW
 // document.write('<span class="cell header">&times;</span>');
-let i = 1;
+let i = 1
 while (i <= 10) {
-    document.write('<span class="cell header">' + i + "</span>");
-    i++;
+    document.write('<span class="cell header">' + i + "</span>")
+    i++
 }
+
 // OUTPUT EACH BODY ROWS
-document.write('<span class="cell header"><br></span>');
-// 1: INITIALIZE A LOOP VARIABLE
-// 2: CONDITIONAL (EXIT PLAN)
-// 3: INCREMENT THE LOOP VARIABLE
+document.write('<span class="cell header"><br></span>')
 let rowNum = 0
 document.write('<br>')
 while (rowNum <= 10) {
-
     let sum
-
     let columnNum = 1
     while (columnNum <= 10) {
         sum = rowNum * columnNum
         document.write('<span class="cell">' + sum + '</span>')
         columnNum++
     }
-
-
     document.write('<br>')
-
-
     rowNum++
 }
