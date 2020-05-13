1983d7679f1f9a6b84d306f1e0e910a78bd0eaaf
Joseph Dubon <josephdubon@pm.me>
2020-05-13 11:06:15 -0700

    Completed multiplication table with reference from Randy's video. Updates to index.html and main.js

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
 // OUTPUT THE HEADER ROW
-document.write('<span class="cell header">&times;</span>');
-let i = 0;
+// document.write('<span class="cell header">&times;</span>');
+let i = 1;
 while (i <= 10) {
     document.write('<span class="cell header">' + i + "</span>");
     i++;
 }
 // OUTPUT EACH BODY ROWS
-
+document.write('<span class="cell header"><br></span>');
 // 1: INITIALIZE A LOOP VARIABLE
 // 2: CONDITIONAL (EXIT PLAN)
 // 3: INCREMENT THE LOOP VARIABLE
-
-let rowNum = 1
+let rowNum = 0
+document.write('<br>')
 while (rowNum <= 10) {
-    document.write('<span class="cell header">' + rowNum + '</span>')
+
+    let sum
+
+    let columnNum = 1
+    while (columnNum <= 10) {
+        sum = rowNum * columnNum
+        document.write('<span class="cell">' + sum + '</span>')
+        columnNum++
+    }
+
+
     document.write('<br>')
+
+
     rowNum++
 }
-
-
-// let j = 1
-
-// for (i = 0; i <= 10; i++) {
-//     for (j = 0; j <= 10; j++) {
-//         document.write('<span class="cell">' + j * i + "</span>")
-//         }
-//         document.write("<br>")
-//     }
